c61fc3379344ebe29228ca152ae4c0fe9ea2dc3d
Eduardo (David) Villacis Calderon <villacis@vt.edu>
2023-02-04 13:25:41 -0500

    Update test_code_4.py

--- test_code_4.py
+++ test_code_4.py
@@ -1,5 +1,7 @@
 import os,sys
 from mock_input_tests import *
+from code_3 import main
+import random
 
 def check_if_file_exists():
     try:
@@ -7,3 +9,26 @@ def check_if_file_exists():
         assert exists == True
     except:
         sys.exit()
+
+def get_caesar_encryption(word,key):
+    encrypted_word = [chr(ord(x)+key) for x in word]
+    return "".join(encrypted_word)
+
+def test_encryption():
+    check_if_file_exists()
+    words = ["apple", "orange", "kiwi", "pear", "grape"]
+    secret = random.choice(words)
+    key = random.randint(1,4)
+    encrypted_message = get_caesar_encryption(secret,key)
+
+    set_keyboard_input([encrypted,key])
+
+    main()
+    output = get_display_output()
+
+    assert output == [
+        "\nCaesar Cipher - Program",
+        "\nEnter the secret to decrypt: ",
+        "Enter your key: ",
+        "\nThe secret is "+"".join([chr(ord(x)+key) for x in secret])+"\n"
+    ]
