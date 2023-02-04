e07a04d2293d66420c77f3bd1c2cb0986c74c01b
Eduardo (David) Villacis Calderon <villacis@vt.edu>
2023-02-04 13:37:01 -0500

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -1,6 +1,16 @@
 
 def main():
-   pass
+   print("\nCaesar Cipher - Program")
+   word = input("\nEnter the secret to decrypt: ")
+   key = int(input("Enter your key: "))
+  
+   secret = [chr(ord(x)-key) for x in word]
+   secret = "".join(secret)
+
+   # for c in word:
+   #    encrypted_word += chr(ord(c)+1)
+   
+   print(f"\nThe secret is {secret}\n")
     
 
 if __name__ == "__main__":
